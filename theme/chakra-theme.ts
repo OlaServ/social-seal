ecf112ebbbd23082ade294c18a25a8140fabfc4f
OlaServ <aleksandra.serwotka@gmail.com>
2024-02-05 11:10:44 +0100

    added fonts and colors to chakra theme

---
+++ theme/chakra-theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from "@chakra-ui/react";
+import { colors } from "./colors";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ weight: ["200", "400", "600", "800"] });
+
+export const ChakraTheme = extendTheme({
+  global: {
+    html: {
+      fontFamily: poppins.style.fontFamily,
+    },
+  },
+  colors,
+  fonts: {
+    heading: poppins.style.fontFamily,
+    body: poppins.style.fontFamily,
+  },
+
+  components: {},
+});
---
+++ theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: {
+    orange: "#F89E52",
+    blue: "#3482FE",
+    blueDark: "#292F3E",
+    black: "#000000",
+    white: "#FFFFFF",
+    gray: "#727272",
+    yellow: "#FFD437",
+  },
+
+  secondary: {
+    black: "#252541",
+    gray: "#E5E7EC",
+    blue: "#3482FF",
+  },
+};
