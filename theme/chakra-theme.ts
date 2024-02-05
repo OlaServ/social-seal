ebf8fced7a597db4b2d766cb78ee709a64edc3d6
OlaServ <aleksandra.serwotka@gmail.com>
2024-02-05 11:26:25 +0100

    set up theme provider

--- theme/chakra-theme.ts
+++ theme/chakra-theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import { colors } from "./colors";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["200", "400", "600", "800"] });
+const poppins = Poppins({ weight: ["200", "400", "600", "800"], subsets: ['latin'] });
 
 export const ChakraTheme = extendTheme({
   global: {
@@ -18,3 +18,4 @@ export const ChakraTheme = extendTheme({
 
   components: {},
 });
+
